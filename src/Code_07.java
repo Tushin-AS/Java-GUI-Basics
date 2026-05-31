687cab4466f1304e65c16d40c0bd3ee08b4578ad
Tushin-AS <abrarshariftushin003@outlook.com>
2026-06-01 00:51:26 +0600

    June 01st.

--- src/Code_07.java
+++ src/Code_07.java
@@ -1,16 +1,17 @@
 // ---- BorderLayout ----
+// Part 2
 
 import javax.swing.*;
 import java.awt.*;
 
-public class Code_06 {
+public class Code_07 {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
 
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500,500);
-        frame.setLayout(new BorderLayout(10,10));
+        frame.setLayout(new BorderLayout());
 
         JPanel panel1 = new JPanel();
         JPanel panel2 = new JPanel();
@@ -24,11 +25,41 @@ public class Code_06 {
         panel4.setBackground(Color.MAGENTA);
         panel5.setBackground(Color.BLUE);
 
+        panel5.setLayout(new BorderLayout());
+
         panel1.setPreferredSize(new Dimension(100,50));
         panel2.setPreferredSize(new Dimension(50,100));
         panel3.setPreferredSize(new Dimension(50,100));
         panel4.setPreferredSize(new Dimension(100,50));
         panel5.setPreferredSize(new Dimension(100,100));
+
+        // ---- sub panels ----
+        JPanel panel6 = new JPanel();
+        JPanel panel7 = new JPanel();
+        JPanel panel8 = new JPanel();
+        JPanel panel9 = new JPanel();
+        JPanel panel10 = new JPanel();
+
+        panel6.setBackground(Color.BLACK);
+        panel7.setBackground(Color.DARK_GRAY);
+        panel8.setBackground(Color.GRAY);
+        panel9.setBackground(Color.LIGHT_GRAY);
+        panel10.setBackground(Color.WHITE);
+
+        panel5.setLayout(new BorderLayout());
+
+        panel6.setPreferredSize(new Dimension(50,50));
+        panel7.setPreferredSize(new Dimension(50,50));
+        panel8.setPreferredSize(new Dimension(50,50));
+        panel9.setPreferredSize(new Dimension(50,50));
+        panel10.setPreferredSize(new Dimension(50,50));
+        // ---- sub panels ----
+
+        panel5.add(panel6,BorderLayout.NORTH);
+        panel5.add(panel7,BorderLayout.SOUTH);
+        panel5.add(panel8,BorderLayout.EAST);
+        panel5.add(panel9,BorderLayout.WEST);
+        panel5.add(panel10,BorderLayout.CENTER);
 
         frame.add(panel1,BorderLayout.NORTH);
         frame.add(panel2,BorderLayout.WEST);
